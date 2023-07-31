2375fd8aec75a7980f6e9c92084f1bc4478f3cb8
PolyakovSergey <ancientcult89@gmail.com>
2023-07-31 21:57:43 +0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import  {  Button, Space  } from "antd";
+function App() {
+  return (
+      <Space wrap>
+        <Button type="primary">Ant Button</Button>
+      </Space>
+  );
+}
+
+export default App;
